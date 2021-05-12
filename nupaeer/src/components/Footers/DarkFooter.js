c60d73acbe5e4769335e56adb6684558426cbe83
will-lzx <will-liu@live.com>
2021-05-12 11:29:34 +0800

    update footer

--- nupaeer/src/components/Footers/DarkFooter.js
+++ nupaeer/src/components/Footers/DarkFooter.js
@@ -9,11 +9,12 @@ function DarkFooter() {
     <footer className="footer" data-background-color="black">
       <Container>
         <div className="copyright" id="copyright">
+          沪ICP备2021012993号-1 
           © {new Date().getFullYear()}, Designed by{" "}
           <a
             href="/"
           >
-            上海安蕊国际贸易有限公司
+            上海努佩尔科技有限公司
           </a>
         </div>
       </Container>
