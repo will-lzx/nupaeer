69783544b02a565e36d965244406831127bd5a5b
will-lzx <will-liu@live.com>
2021-05-14 12:28:18 +0800

    update footer

--- nupaeer/src/components/Footers/DarkFooter.js
+++ nupaeer/src/components/Footers/DarkFooter.js
@@ -8,6 +8,14 @@ function DarkFooter() {
   return (
     <footer className="footer" data-background-color="black">
       <Container>
+        <div className="contact">
+          销售总监<br/>
+          13761709038
+          
+        </div>
+      </Container>
+      <div>
+        <img className="footer-img" src={ require("assets/img/WechatIMG84.jpeg") } alt="..."></img>
         <div className="copyright" id="copyright">
           沪ICP备2021012993号-1<br/>
           © {new Date().getFullYear()}  {" "}
@@ -17,7 +25,8 @@ function DarkFooter() {
             上海努佩尔科技有限公司
           </a>
         </div>
-      </Container>
+      </div>
+      
     </footer>
   );
 }
