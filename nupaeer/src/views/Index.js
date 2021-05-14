a8b71a40e3b0df40e683d9e4ed08cdfd68230652
will-lzx <will-liu@live.com>
2021-05-14 11:47:26 +0800

    update introduce

--- nupaeer/src/views/Index.js
+++ nupaeer/src/views/Index.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 // reactstrap components
-// import {
-// } from "reactstrap";
+import {
+  Row,
+  Col
+} from "reactstrap";
 
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
@@ -29,8 +31,18 @@ function Index() {
         <IndexHeader />
         <div className="section">
           <div className="container">
-            <h3 className="title">产品介绍</h3>
-            <Carousel></Carousel>
+          <Row className="justify-content-md-center">
+              <Col lg="8" md="12">
+              <h3 className="title">产品介绍</h3>
+              <h4 className="title">日本品牌：Leadsun</h4>
+              <h5>0尼古丁电子口香糖</h5>
+              <h5>Leadsun one系列</h5>
+              <h5>套装零售价188元（一个烟杆+3个烟弹）</h5>
+              <h5>单口味烟弹3个一套零售价88元</h5>
+              <h5>6种不同口味烟弹一套零售价128元</h5>
+              </Col>
+          </Row>
+          <Carousel></Carousel>
           </div>
         </div>
         <DarkFooter />
