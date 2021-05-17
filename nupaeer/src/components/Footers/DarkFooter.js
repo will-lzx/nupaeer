9df83180d01c714a3c9414bc98e65fc49b8d4346
will-lzx <will-liu@live.com>
2021-05-17 11:14:43 +0800

    update style

--- nupaeer/src/components/Footers/DarkFooter.js
+++ nupaeer/src/components/Footers/DarkFooter.js
@@ -8,25 +8,16 @@ function DarkFooter() {
   return (
     <footer className="footer" data-background-color="black">
       <Container>
-        <div className="contact">
-          销售总监<br/>
-          13761709038
-          
-        </div>
-      </Container>
-      <div>
-        <img className="footer-img" src={ require("assets/img/WechatIMG84.jpeg") } alt="..."></img>
         <div className="copyright" id="copyright">
-          沪ICP备2021012993号-1<br/>
-          © {new Date().getFullYear()}  {" "}
-          <a
-            href="/"
-          >
-            上海努佩尔科技有限公司
-          </a>
-        </div>
-      </div>
-      
+            沪ICP备2021012993号-1<br/>
+            © {new Date().getFullYear()}  {" "}
+            <a
+              href="/"
+            >
+              上海努佩尔科技有限公司
+            </a>
+          </div>
+      </Container>
     </footer>
   );
 }
